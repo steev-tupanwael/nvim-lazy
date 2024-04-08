b84c0933732315d6d4b1ac691a3362765affc430
steev-tupanwael <steev_tupanwael@outlook.com>
2024-04-09 04:01:22 +0900

    add git, indentline, keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -31,7 +31,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- git
 vim.keymap.set("n", "<leader>gc", ":Git commit -m \"")
-vim.keymap.set("n", "<leader>gp", ":Git push -u origin HEAD<CR>")
+vim.keymap.set("n", "<leader>gP", ":Git push -u origin HEAD<CR>")
 
 -- yank
 vim.keymap.set("n", "<leader>y", "\"+y")
